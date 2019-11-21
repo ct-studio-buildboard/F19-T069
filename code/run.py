1a64cc4203ecbf708e6ecf2c7dc07f691b29de71
Amanda Trang <at669@cornell.edu>
2019-11-21 13:27:32 -0500

    added gitignore

--- code/run.py
+++ code/run.py
@@ -1,5 +1,6 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
+import users
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -8,31 +9,25 @@ SECRET_KEY = 'a secret key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# Try adding your own number to this list!
-callers = {
-    "+18018396027": "Amanda",
-    "+12349013030": "Finn",
-    "+12348134522": "Chewy",
-}
-
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     counter = session.get('counter', 0)
     counter += 1
     session['counter'] = counter
 
-    number = request.form['From']
     message_body = request.form['Body']
     from_number = request.values.get('From')
     
+    if from_number in users._USERS:
+        name = users._USERS[from_number]
+    else:
+        name = "Friend"
+
     if (message_body == "RESET"):
         counter = 0
         session['counter'] = counter
-    
-    if from_number in callers:
-        name = callers[from_number]
     else:
-        name = "Friend"
+        populateInfo(counter, name, message_body)
 
     message = '{} has messaged {} times, last text said "{}"' \
         .format(name, counter, message_body)
@@ -40,6 +35,10 @@ def sms_reply():
     resp = MessagingResponse()
     resp.message(message)
     return str(resp)
+
+def populateInfo(counter, name, message):
+    if (name in users._USERS):
+        users._INFO[name][counter] = message
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ code/users.py
@@ -0,0 +1,12 @@
+_USERS = {
+    "+18018396027": "Amanda",
+    "+12349013030": "Finn",
+    "+12348134522": "Chewy",
+}
+
+# name, age
+_INFO = {
+    "Amanda"    :   [] * 2
+}
+
+print(_INFO)
