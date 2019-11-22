533ec0aeb1cde9b3c5b010ca187e65db37c7cb5f
Amanda Trang <at669@cornell.edu>
2019-11-21 19:45:02 -0500

    Fulfills profile

--- code/run.py
+++ code/run.py
@@ -9,8 +9,14 @@ SECRET_KEY = 'a secret key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+category = ""
+industries = []
+list_jobs = []
+tmp_skills = []
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
+    global list_jobs, tmp_skills, industries, category
     counter = session.get('counter', 0)
     counter += 1
     session['counter'] = counter
@@ -18,36 +24,63 @@ def sms_reply():
     message_body = request.form['Body']
     from_number = request.values.get('From')
     
-    # if from_number in database._USERS:
-    #     name = database._USERS[from_number]
-    # else:
-        # name = message_body.strip()
-
     if (message_body == "RESET"):
         counter = 0
         session['counter'] = counter
 
     if (session['counter'] == 0):
-        message = "Looks like you're a new user! What is your name?"
+        message = "\nLooks like you're a new user! What is your name?"
     elif (session['counter'] == 1):
         database._USERS[from_number] = [message_body.strip(), []]
-        print(database._USERS)
-        message = ("Thanks {}! Let's create your profile. What are your skills?").format(database._USERS[from_number][0])
+        message = ("\nThanks {}! Let's create your profile. Which category would you like to fill out first?\n1: MANUAL\n2: TECHNICAL\n3: PROFESSIONAL").format(database._USERS[from_number][0])
     elif (session['counter'] == 2):
-        skills = message_body.split(",")
-        print(database._USERS[from_number])
-        database._USERS[from_number][1].extend(skills)
+        if (int(message_body.strip()) == 1): # MANUAL
+            category = "MANUAL"
+        elif (int(message_body.strip()) == 2): # TECHNICAL
+            category = "TECHNICAL"
+        elif (int(message_body.strip()) == 3): # PROFESSIONAL
+            category = "TECHNICAL"
+        list_jobs = database._JOBS[category] # get JOBS in CATEGORY
+        msg = "\nChoose an industry to select your skills (you'll be able to come back to these later!):\n"
+        c = 1
+        # print(list_jobs)
+        for ind in list_jobs:
+            msg += str(c) + ": " + ind + "\n"
+            print(ind)
+            industries.append(ind)
+            c += 1
+        message = msg
+        print("industries", industries)
+    elif (session['counter'] == 3):
+        job = industries[int(message_body.strip()) - 1] # construction
+        msg = "\n List your skills in this industry:\n"
+        c = 1
+        tmp_skills = []
+        for s in database._JOBS[category][job]:
+            msg += str(c) + ": " + s + "\n"
+            tmp_skills.append(s)
+            c += 1
+        message = msg
+    elif (session['counter'] == 4): # skills selected
+        user_skill = message_body.replace(" ", "").split(",")
+        for i in range(len(user_skill)):
+            user_skill[i] = int(user_skill[i])
+        print("hello?")
+        print(len(user_skill), user_skill, tmp_skills)
+        for i in range(len(user_skill)):
+            print(i)
+            database._USERS[from_number][1].append(tmp_skills[i])
         message = ("{}, you've said your skills are: {}").format(database._USERS[from_number][0], database._USERS[from_number][1])
+        print("tmp skills ", type(tmp_skills[i-1]), tmp_skills[i-1])
+        print(database._USERS[from_number][1])
+        message = message + "\nEnter:\n1: To go back to industries\n2: To save and exit profile building"
+    elif (session['counter'] == 5): # save and EXIT
+        if (int(message_body.strip()) == 2): 
+            message = "\nYou've completed your profile!\nHere's the jobs that match your profile:\n"
 
     resp = MessagingResponse()
     resp.message(message)
     return str(resp)
 
-# def populateInfo(counter, name, message):
-#     if ( name in users._USERS):
-#         users._INFO[name][counter] = message
-
 if __name__ == "__main__":
-    job_input.createJob()
-    print("Job created!")
     app.run(debug=True)
