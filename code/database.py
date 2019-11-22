d9c68737e9910733bd601c94311decd6f30037dc
Amanda Trang <at669@cornell.edu>
2019-11-21 21:09:57 -0500

    Full profile (can go through all categories)

--- code/database.py
+++ code/database.py
@@ -1,16 +1,22 @@
 from enum import Enum
 
 _USERS = {
-    "8018396027" : ["Amanda", ["power tools", "heavy lifting"]]
+
 }
 
 _JOBS = {
     "MANUAL" : {
         "construction" : {"heavy lifting", "power tools"},
-        "agriculture" : {"harvesting"}
+        "agriculture" : {"harvesting", "crop growing", "livestock farming", "poultry farming", "shepherding"}
     },
-    "TECHNICAL" : {},
-    "PROFESSIONAL" : {},
+    "TECHNICAL" : {
+        "coding" : {"Python", "MATLAB", "C++", "C", "javascript", "PHP", "HTML", "SQL", "R", "C#"},
+        "MS Office" : {"Excel", "Word", "PowerPoint", "Outlook"}
+    },
+    "PROFESSIONAL" : {
+        "medical" : {"therapy", "physical therapy", "dentistry", "gynecology", "pharmacy"},
+        "education" : {"teaching", "tutoring", "school application advising", "curriculum designing"}
+    },
 }
 
 _TAGS = {
--- code/run.py
+++ code/run.py
@@ -1,6 +1,6 @@
 # /usr/bin/env python
 # Download the twilio-python library from twilio.com/docs/libraries/python
-import database, job_input
+import database
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -14,44 +14,53 @@ industries = []
 list_jobs = []
 tmp_skills = []
 
+_FLOW = ["GET_NAME", "CHOOSE_CATEGORY", "LIST_SKILLS", "NEXT_NAV", "GET_JOBS"]
+_IDX = 0
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    global list_jobs, tmp_skills, industries, category
-    counter = session.get('counter', 0)
-    counter += 1
-    session['counter'] = counter
+    global list_jobs, tmp_skills, industries, category, _FLOW, _IDX
+    _IDX = session.get('_IDX', 0)
+    session['_IDX'] = _IDX
 
     message_body = request.form['Body']
     from_number = request.values.get('From')
     
     if (message_body == "RESET"):
-        counter = 0
-        session['counter'] = counter
+        _IDX = 0
+        session['_IDX'] = 0
 
-    if (session['counter'] == 0):
+    if (session['_IDX'] == 0):
         message = "\nLooks like you're a new user! What is your name?"
-    elif (session['counter'] == 1):
-        database._USERS[from_number] = [message_body.strip(), []]
+        _IDX = 1
+    elif (session['_IDX'] == 1):
+        if (from_number not in database._USERS):
+            database._USERS[from_number] = [message_body.strip(), []]
         message = ("\nThanks {}! Let's create your profile. Which category would you like to fill out first?\n1: MANUAL\n2: TECHNICAL\n3: PROFESSIONAL").format(database._USERS[from_number][0])
-    elif (session['counter'] == 2):
+        _IDX = 2
+    elif (session['_IDX'] == 2):
+        msg = "\nChoose an industry to select your skills (you'll be able to come back to these later!):\n"
         if (int(message_body.strip()) == 1): # MANUAL
             category = "MANUAL"
+            _IDX = 3
         elif (int(message_body.strip()) == 2): # TECHNICAL
             category = "TECHNICAL"
+            _IDX = 3
         elif (int(message_body.strip()) == 3): # PROFESSIONAL
-            category = "TECHNICAL"
-        list_jobs = database._JOBS[category] # get JOBS in CATEGORY
-        msg = "\nChoose an industry to select your skills (you'll be able to come back to these later!):\n"
-        c = 1
-        # print(list_jobs)
-        for ind in list_jobs:
-            msg += str(c) + ": " + ind + "\n"
-            print(ind)
-            industries.append(ind)
-            c += 1
-        message = msg
-        print("industries", industries)
-    elif (session['counter'] == 3):
+            category = "PROFESSIONAL"
+            _IDX = 3
+        else:
+            message = "Sorry, try again."
+        if (int(message_body.strip()) <= 3):
+            industries = []
+            list_jobs = database._JOBS[category] # get JOBS in CATEGORY
+            c = 1
+            for ind in list_jobs:
+                msg += str(c) + ": " + ind + "\n"
+                industries.append(ind)
+                c += 1
+            message = msg
+    elif (session['_IDX'] == 3):
         job = industries[int(message_body.strip()) - 1] # construction
         msg = "\n List your skills in this industry:\n"
         c = 1
@@ -61,23 +70,28 @@ def sms_reply():
             tmp_skills.append(s)
             c += 1
         message = msg
-    elif (session['counter'] == 4): # skills selected
+        _IDX = 4
+    elif (session['_IDX'] == 4): # skills selected
         user_skill = message_body.replace(" ", "").split(",")
         for i in range(len(user_skill)):
-            user_skill[i] = int(user_skill[i])
-        print("hello?")
-        print(len(user_skill), user_skill, tmp_skills)
+            user_skill[i] = int(user_skill[i]) - 1
         for i in range(len(user_skill)):
-            print(i)
-            database._USERS[from_number][1].append(tmp_skills[i])
-        message = ("{}, you've said your skills are: {}").format(database._USERS[from_number][0], database._USERS[from_number][1])
-        print("tmp skills ", type(tmp_skills[i-1]), tmp_skills[i-1])
-        print(database._USERS[from_number][1])
+            database._USERS[from_number][1].append(tmp_skills[user_skill[i]])
+        str_skills = ""
+        for i in range(len(database._USERS[from_number][1])):
+            str_skills += database._USERS[from_number][1][i] + ", "
+        message = ("{}, you've said your skills are: {}").format(database._USERS[from_number][0], str_skills[:len(str_skills) - 2])
         message = message + "\nEnter:\n1: To go back to industries\n2: To save and exit profile building"
-    elif (session['counter'] == 5): # save and EXIT
-        if (int(message_body.strip()) == 2): 
-            message = "\nYou've completed your profile!\nHere's the jobs that match your profile:\n"
+        _IDX = 5
+    elif (session['_IDX'] == 5): # save and EXIT
+        if (int(message_body.strip()) == 1): 
+            _IDX = 1
+            message = "\nBringing you back to the industries list... Reply to proceed.\n"
+        elif (int(message_body.strip()) == 2): 
+            _IDX = 6
+            message = "\nYou've completed your profile!\nFinding the jobs that match...\n"
 
+    session['_IDX'] = _IDX
     resp = MessagingResponse()
     resp.message(message)
     return str(resp)
